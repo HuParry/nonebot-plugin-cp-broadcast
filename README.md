84c4dc9cf20e15aaee69be7d46a02ec345b9a6c4
HuParry <huparry@outlook.com>
2023-07-10 23:28:56 +0800

    修改config

--- README.md
+++ README.md
@@ -89,7 +89,7 @@ _✨ 一个 Codeforces、牛客竞赛、AtCoder 平台的编程竞赛查询插�
 | cp_broadcast_time | 否 | {"hour":"7", "minute":"20"} | 每日在群聊播报比赛信息的时间，默认是早上 7 点 20，你可以在配置文件中按默认值格式修改成你想要的时间 |
 | cp_broadcast_updatetime | 否 | {"hour":"0", "minute":"0"} | 每天自动更新比赛数据的时间，默认是 0 点 0 分，你可以在配置文件中按默认值格式修改成你想要的时间 |
 
-该插件依赖 [nonebot_plugin_apscheduler](https://github.com/nonebot/plugin-apscheduler) 实现定时发送功能，如果你是通过下载项目源码安装插件的话，请注意安装好依赖。
+该插件依赖 [nonebot_plugin_apscheduler](https://github.com/nonebot/plugin-apscheduler) 实现定时发送功能，如果你未安装此依赖的话，将无法触发与定时相关的功能。
 
 ## 🎉 使用
 ### 指令表
--- nonebot_plugin_cp_broadcast/config.py
+++ nonebot_plugin_cp_broadcast/config.py
@@ -1,12 +1,12 @@
 from pydantic import BaseModel, Extra
-from typing import List
+from typing import List, Dict
 
 from nonebot import get_driver
 
 class Config(BaseModel, extra=Extra.ignore):
     cp_broadcast_list: List[str] = []
     cp_broadcast_botname: str = "bot"
-    cp_broadcast_time: dict[str, str] = {"hour" : "7", "minute" : "20"}
-    cp_broadcast_updatetime: dict[str, str] = {"hour" : "0", "minute" : "0"}
+    cp_broadcast_time: Dict[str, str] = {"hour" : "7", "minute" : "20"}
+    cp_broadcast_updatetime: Dict[str, str] = {"hour" : "0", "minute" : "0"}
 
 cp_broadcast_config = Config.parse_obj(get_driver().config.dict())
