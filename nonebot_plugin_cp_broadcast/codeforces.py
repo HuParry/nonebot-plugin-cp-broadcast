d6bdda6ff5d55cc8596b33ddf4cdd47c4f241abb
HuParry <hu_parry@qq.com>
2026-05-13 20:55:25 +0800

    修复cf查询用户失败的bug

--- nonebot_plugin_cp_broadcast/codeforces.py
+++ nonebot_plugin_cp_broadcast/codeforces.py
@@ -150,7 +150,7 @@ async def query_handle(bot: Bot, event: GroupMessageEvent, args: Message = Comma
         msg = await queryUser(cfid)
         await query.finish(msg)
     else:
-        await bind.finish('绑定失败，请按照格式发起指令！')
+        await query.finish('查询失败，请确认用户是否真实存在！')
 
 
 @rank.handle()
